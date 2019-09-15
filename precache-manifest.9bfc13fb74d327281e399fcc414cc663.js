86153bfe61bc0c8cfe47192f5dc7b39469d55e1a
nelson.oliveira <nelsonlpco@gmail.com>
2019-09-15 12:09:09 -0300

    Updates

---
+++ precache-manifest.9bfc13fb74d327281e399fcc414cc663.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42bff41aa8a029e351e054cf945a717e",
+    "url": "/round-count/index.html"
+  },
+  {
+    "revision": "c37570a958bb41829ca3",
+    "url": "/round-count/static/js/2.c9d9e182.chunk.js"
+  },
+  {
+    "revision": "2e34dcdfd174c1b231b8",
+    "url": "/round-count/static/js/main.f967e2dc.chunk.js"
+  },
+  {
+    "revision": "9bc13e3ecd91af2088eb",
+    "url": "/round-count/static/js/runtime~main.c4e95068.js"
+  }
+]);
--- src/store/round-manager/model.ts
+++
@@ -1,9 +0,0 @@
-export interface Round {
-    rounds: number,
-    roundTime: number,
-    currentTime: number,
-    currentRound: number,
-    currentInterval: number,
-    interval: number,
-    started: boolean,
-};
